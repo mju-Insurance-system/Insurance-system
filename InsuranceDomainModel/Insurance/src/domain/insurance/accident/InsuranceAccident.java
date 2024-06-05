772dd95b270dcd2a1274811e5bd82f32a07e1d09
kimssoyeon <122100002+kimssoyeon@users.noreply.github.com>
2024-06-05 16:05:35 +0900

    김소연 Update InsuranceAccident commit(get,set,메소드 내용)

--- InsuranceDomainModel/Insurance/src/domain/insurance/accident/InsuranceAccident.java
+++ InsuranceDomainModel/Insurance/src/domain/insurance/accident/InsuranceAccident.java
@@ -1,20 +1,13 @@
-package domain.insurance.accident;
-
-import domain.insurance.InsuranceCalculator;
-import domain.insurance.InsuranceClaim;
-import domain.user.Investigator;
-import domain.insurance.PaymentApprovalLetter;
-import domain.insurance.Insurance;
-import domain.user.User;
-import domain.user.staff.RiskCoveringTeam;
+package domain;
 
 import java.io.File;
 import java.util.Date;
 
 public class InsuranceAccident {
 
-	private EAccessibleAuthority[] accessibleAuthority;
-	private boolean bConclude;
+	private EAccessableAuthority[] accessableAuthority;
+
+	private boolean bConclud;
 	private boolean bInvestigation;
 	private String certificationCode;
 	private Insurance claimedInsurance;
@@ -36,17 +29,162 @@ public class InsuranceAccident {
 	public InsuranceAccident(){
 
 	}
+	
 
-	public void finalize() throws Throwable {
-
+	public EAccessableAuthority[] getAccessableAuthority() {
+		return accessableAuthority;
 	}
 
-	/**
-	 * 
-	 * @param investigator
-	 */
-	public void appointInvestigator(Investigator investigator){
+	public void setAccessableAuthority(EAccessableAuthority[] accessableAuthority) {
+		this.accessableAuthority = accessableAuthority;
+	}
 
+	public boolean isbConclud() {
+		return bConclud;
+	}
+
+	public void setbConclud(boolean bConclud) {
+		this.bConclud = bConclud;
+	}
+
+	public boolean isbInvestigation() {
+		return bInvestigation;
+	}
+
+	public void setbInvestigation(boolean bInvestigation) {
+		this.bInvestigation = bInvestigation;
+	}
+
+	public String getCertificationCode() {
+		return certificationCode;
+	}
+
+	public void setCertificationCode(String certificationCode) {
+		this.certificationCode = certificationCode;
+	}
+
+	public Insurance getClaimedInsurance() {
+		return claimedInsurance;
+	}
+
+	public void setClaimedInsurance(Insurance claimedInsurance) {
+		this.claimedInsurance = claimedInsurance;
+	}
+
+	public double getClaimsReserve() {
+		return claimsReserve;
+	}
+
+	public void setClaimsReserve(double claimsReserve) {
+		this.claimsReserve = claimsReserve;
+	}
+
+	public int getInsuranceAccidentId() {
+		return insuranceAccidentId;
+	}
+
+	public void setInsuranceAccidentId(int insuranceAccidentId) {
+		this.insuranceAccidentId = insuranceAccidentId;
+	}
+
+	public double getInsuranceAmount() {
+		return insuranceAmount;
+	}
+
+	public void setInsuranceAmount(double insuranceAmount) {
+		this.insuranceAmount = insuranceAmount;
+	}
+
+	public File getInvestirgationReport() {
+		return investirgationReport;
+	}
+
+	public void setInvestirgationReport(File investirgationReport) {
+		this.investirgationReport = investirgationReport;
+	}
+
+	public String getLiability() {
+		return liability;
+	}
+
+	public void setLiability(String liability) {
+		this.liability = liability;
+	}
+
+	public String getReporterName() {
+		return reporterName;
+	}
+
+	public void setReporterName(String reporterName) {
+		this.reporterName = reporterName;
+	}
+
+	public String getReporterPhone() {
+		return reporterPhone;
+	}
+
+	public void setReporterPhone(String reporterPhone) {
+		this.reporterPhone = reporterPhone;
+	}
+
+	public int getRiskNumber() {
+		return riskNumber;
+	}
+
+	public void setRiskNumber(int riskNumber) {
+		this.riskNumber = riskNumber;
+	}
+
+	public int getRiskRank() {
+		return riskRank;
+	}
+
+	public void setRiskRank(int riskRank) {
+		this.riskRank = riskRank;
+	}
+
+	public String getRiskType() {
+		return riskType;
+	}
+
+	public void setRiskType(String riskType) {
+		this.riskType = riskType;
+	}
+
+	public RiskCoveringTeam getStaffInCharge() {
+		return staffInCharge;
+	}
+
+	public void setStaffInCharge(RiskCoveringTeam staffInCharge) {
+		this.staffInCharge = staffInCharge;
+	}
+
+	public InsuranceCalculator getM_InsuranceCalculator() {
+		return m_InsuranceCalculator;
+	}
+
+	public void setM_InsuranceCalculator(InsuranceCalculator m_InsuranceCalculator) {
+		this.m_InsuranceCalculator = m_InsuranceCalculator;
+	}
+
+	public PaymentApprovalLetter getM_PaymentApprovalLetter() {
+		return m_PaymentApprovalLetter;
+	}
+
+	public void setM_PaymentApprovalLetter(PaymentApprovalLetter m_PaymentApprovalLetter) {
+		this.m_PaymentApprovalLetter = m_PaymentApprovalLetter;
+	}
+
+	public InsuranceClaim getM_InsuranceClaim() {
+		return m_InsuranceClaim;
+	}
+
+	public void setM_InsuranceClaim(InsuranceClaim m_InsuranceClaim) {
+		this.m_InsuranceClaim = m_InsuranceClaim;
+	}
+	
+	public void appointInvestigator(Investigator investigator){
+		
 	}
 
 	public RiskCoveringTeam assignAccident(){
@@ -57,46 +195,35 @@ public class InsuranceAccident {
 
 	}
 
-	/**
-	 * 
-	 * @param authority
-	 */
-	public boolean certifyCode(int authority){
-		return false;
+
+	
+	public boolean certifyCode(String authority){//String으로 바꿨어요
+		for (EAccessableAuthority auth : accessableAuthority) {
+            if (auth.equals(authority)) return true;
+        }
+        return false;
 	}
 
-	/**
-	 * 
-	 * @param accessing user
-	 */
 	public boolean checkAuthority(User accessingUser){
-		return false;
+		for (EAccessableAuthority auth : accessableAuthority) {
+            if (auth == accessingUser.getaccessAuthority()) { //여기 User에서 get, set 해줘야해요
+                return true;
+            }
+        }
+        return false;
 	}
 
-	/**
-	 * 
-	 * @param claim
-	 */
 	public void claim(InsuranceClaim claim){
-
 	}
 
 	public boolean confirmInsured(){
 		return false;
 	}
 
-	/**
-	 * 
-	 * @param rank
-	 */
 	public void determineRank(int rank){
-
+		this.riskRank = rank;
 	}
 
-	/**
-	 * 
-	 * @param reason
-	 */
 	public void dismissClaim(String reason){
 
 	}
@@ -105,31 +232,23 @@ public class InsuranceAccident {
 		return null;
 	}
 
-	/**
-	 * 
-	 * @param liability
-	 */
+
 	public void judgeLiability(String liability){
-
+		this.liability = liability;
 	}
 
-	/**
-	 * 
-	 * @param pay date
-	 * @param payment approval letter
-	 */
 	public boolean payInsuredAmount(Date payDate, PaymentApprovalLetter paymentApprovalLetter){
-		return false;
+		 if (this.insuranceAmount > 0 && paymentApprovalLetter != null) {
+	            this.m_PaymentApprovalLetter = paymentApprovalLetter;
+	            this.bConclud = true;
+	            System.out.println("Payment approved: " + insuranceAmount + payDate);
+	            return true;
+	        }
+	        return false;
 	}
 
-	/**
-	 * 
-	 * @param claimed
-	 * @param insurance
-	 * @param claim
-	 */
 	public void reviewRisk(User claimed, Insurance insurance, InsuranceClaim claim){
-
+		
 	}
 
 	public File[] searchDocuments(){
@@ -137,7 +256,9 @@ public class InsuranceAccident {
 	}
 
 	public File writeInvestigationReport(){
-		return null;
+		this.investirgationReport = new File("investigation_report.txt");
+        System.out.println("Investigation report written: " + investirgationReport.getName());
+        return this.investirgationReport;
 	}
 
 }
